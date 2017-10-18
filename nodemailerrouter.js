e5582a0f4215bc90b640483fa66f8fe7545340ae
Zac Mondejar <31014539+zaciscoding@users.noreply.github.com>
2017-10-18 11:30:56 -0700

    Initial commit

---
+++ nodemailerrouter.js
@@ -0,0 +1,53 @@
+// Setup for nodemailer npm module, should email of user recipient, and email pass.
+// should
+
+const express = require('express')
+const router = express.Router()
+const nodemailer = require('nodemailer')
+
+router.get('/contact', function (req, res, next) {
+  res.render('contact', {
+    pageTitle: 'Contact',
+    pageId: 'contact',
+    msg: null,
+    err: null
+  })
+})
+
+router.post('/contactform', function (req, res, next) {
+  let transporter = nodemailer.createTransport({
+
+    host: 'SMTP',
+    service: 'servicegoeshere/example*gmail',
+    port: 587,
+    secure: false,
+    auth: {
+      user: 'emailgoeshere',
+      pass: 'passwordgoeshere'
+    }
+  })
+
+  var mailOpts = {
+    from: req.body.name + '&lt' + req.body.email + '&gt',
+    to: 'who is getting email',
+    subject: 'title',
+    text: req.body.message + req.body.email }
+
+  transporter.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.render('contact', {
+        pageTitle: 'Contact',
+        pageId: 'contact',
+        msg: null,
+        err: 'Error Occurred: message was not sent.'})
+    } else {
+      console.log(' message has been sent')
+      res.render('contact', {
+        pageTitle: 'Contact',
+        pageId: 'contact',
+        msg: 'Success.',
+        err: null })
+    }
+  })
+})
+module.exports = router
